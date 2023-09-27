d6fcd93188998bc210be379d31c5ac5d561990ea
master-jedi365 <spacecats365@gmail.com>
2023-09-27 18:52:52 +0900

    重線型回帰追加

--- src/multi_linear_regression.py
+++ src/multi_linear_regression.py
@@ -8,12 +8,12 @@ from sklearn.linear_model import LinearRegression
 sys.path.append(os.path.join("src", "utils"))
 from data_utils import get_calif_dataset, get_data_from_name
 
-OUTPUT_NAME="simple_linear_result"
+OUTPUT_NAME="multi_linear_result"
 
 def arg_parse():
     ap = argparse.ArgumentParser()  
     ap.add_argument('--tran_test_split_ratio', type=float, default=1/10)
-    ap.add_argument('--exp_name_str', type=str, default="MedInc", choices=['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup', 'Latitude', 'Longitude'])  
+    ap.add_argument('--exp_names_str_list', type=str, nargs="*")  
     ap.add_argument('--output_dir_path', type=str, default="workdir")  
     return ap.parse_args()    
 
@@ -23,26 +23,23 @@ def main():
     calif_ds = get_calif_dataset(args.tran_test_split_ratio)
     lr = LinearRegression()
 
-    # 一次元線形回帰
-    train_X, test_X = get_data_from_name([args.exp_name_str], calif_ds)
+    # 重線形回帰
+    train_X, test_X = get_data_from_name(args.exp_names_str_list, calif_ds)
     lr.fit(train_X, calif_ds.pred_train_data)
-    a = lr.coef_[0][0]
+    a_list = lr.coef_[0]
     b = lr.intercept_[0]
     R2 = lr.score(test_X, calif_ds.pred_test_data)
 
-    print("a: ", a)
+    print("a: ", a_list)
     print("b: ", b)
     print("test R2: ", R2)
     #出力
-    # visualize
     os.makedirs(args.output_dir_path, exist_ok=True)
-    plt.scatter(test_X.ravel(), calif_ds.pred_test_data.ravel())
-    max_x = max(train_X.ravel())
-    plt.plot([0, max_x], [b, a * max_x + b], color="red")
+    # visualize
+    coef_list = list(map(float, a_list))
+    plt.bar(list(range(0, len(args.exp_names_str_list))), coef_list)
     plt.savefig(os.path.join(args.output_dir_path, f"{OUTPUT_NAME}.jpg"))
-
     # parameters as yaml
-    coef_list = list(map(float, lr.coef_[0]))
     out_dict = {
         "coefs": coef_list,
         "intercept": float(lr.intercept_[0]),
